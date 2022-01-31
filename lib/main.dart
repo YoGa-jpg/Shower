5ef4b17c7e95e86ac7f5f6596a21ebbc6086d9ff
YoGa-jpg <antondavidovich45@gmail.com>
2022-01-31 21:17:13 +0300

    Add button templates

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,38 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+      home: Scaffold(
+          body: Container(
+            alignment: Alignment.center,
+              child:Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+            IconButton(
+            icon: ClipRRect(borderRadius: BorderRadius.circular(20.0),child:Image.asset("assets/images/marci.jpg")),
+            iconSize: 300,
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Image.asset("assets/images/marci.jpg"),
+            iconSize: 300,
+            onPressed: () {},
+          )
+        ],
+      )),
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        backgroundColor: Colors.redAccent
+      ),),
     );
   }
 }
