9e2e90cc4bca82e97951e8b9835e3b92f1d988cc
YoGa-jpg <antondavidovich45@gmail.com>
2022-02-01 22:17:23 +0300

    Link buttons with new page

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,22 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainPage());
+  runApp(const AppRoutes());
+}
+
+class AppRoutes extends StatelessWidget {
+  const AppRoutes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MainPage(),
+        '/search': (context) => const SearchPage()
+      },
+    );
+  }
 }
 
 class MainPage extends StatelessWidget {
@@ -13,26 +28,93 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Container(
+        body: Container(
             alignment: Alignment.center,
-              child:Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-            IconButton(
-            icon: ClipRRect(borderRadius: BorderRadius.circular(20.0),child:Image.asset("assets/images/marci.jpg")),
-            iconSize: 300,
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Image.asset("assets/images/marci.jpg"),
-            iconSize: 300,
-            onPressed: () {},
-          )
-        ],
-      )),
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent
-      ),),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ImageButton(
+                    radius: 14.0,
+                    size: 300,
+                    asset: "assets/images/marci.jpg",
+                    func: () {}),
+                ImageButton(
+                    radius: 14.0,
+                    size: 300,
+                    asset: "assets/images/marci.jpg",
+                    func: () {})
+              ],
+            )),
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: const Text("Bar"),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          scrollDirection: Axis.vertical,
+            children: const [
+          MovieCard(),
+          MovieCard(),
+          MovieCard()
+        ]),
+        appBar: AppBar(title: const Text("Список")),
+      ),
+    );
+  }
+}
+
+class ImageButton extends StatelessWidget {
+  final double radius;
+  final int size;
+  final String asset;
+  final Function func;
+
+  const ImageButton(
+      {Key? key,
+      required this.radius,
+      required this.size,
+      required this.asset,
+      required this.func})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: ClipRRect(
+          borderRadius: BorderRadius.circular(14.0),
+          child: Image.asset("assets/images/marci.jpg")),
+      iconSize: 300,
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const SearchPage();
+        }));
+      },
+    );
+  }
+}
+
+class MovieCard extends StatelessWidget{
+  const MovieCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Container(
+          color: Colors.lightGreen,
+          width: 200,
+          height: 300,
+        )
     );
   }
 }
