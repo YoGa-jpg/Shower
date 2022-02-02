89a90f9fefb38034cf8ce6531e64a17b50801e62
YoGa-jpg <antondavidovich45@gmail.com>
2022-02-02 23:13:50 +0300

    Add list template

--- lib/main.dart
+++ lib/main.dart
@@ -62,12 +62,8 @@ class SearchPage extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: ListView(
-          scrollDirection: Axis.vertical,
-            children: const [
-          MovieCard(),
-          MovieCard(),
-          MovieCard()
-        ]),
+            scrollDirection: Axis.vertical,
+            children: const [MovieCard(), MovieCard(), MovieCard()]),
         appBar: AppBar(title: const Text("Список")),
       ),
     );
@@ -104,17 +100,67 @@ class ImageButton extends StatelessWidget {
   }
 }
 
-class MovieCard extends StatelessWidget{
+class MovieCard extends StatelessWidget {
   const MovieCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: Container(
-          color: Colors.lightGreen,
-          width: 200,
-          height: 300,
-        )
+      child:Container(
+        height: 100,
+        color: Colors.white,
+        child: Row(
+          children: [
+            Center(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Expanded(
+                  child:Image.asset("assets/images/marci.jpg"),
+                  flex:2 ,
+                ),
+              ),
+            ),
+            Expanded(
+              child:Container(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: ListTile(
+                        title: Text("Shape Of You"),
+                        subtitle: Text("Ed Sheeran"),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(
+                            child:Text("PLAY"),
+                            onPressed: ()
+                            {},
+                          ),
+                          SizedBox(width: 8,),
+                          TextButton(
+                            child: Text("ADD TO QUEUE"),
+                            onPressed: (){},
+                          ),
+                          SizedBox(width: 8,)
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              flex:8 ,
+            ),
+          ],
+        ),
+      ),
+      elevation: 8,
+      margin: EdgeInsets.all(10),
     );
   }
 }
