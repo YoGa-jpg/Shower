1449ec69b6d3f424365165bb5d1cb0ffbecee1c8
YoGa-jpg <antondavidovich45@gmail.com>
2022-02-06 21:23:55 +0300

    Minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -37,28 +37,34 @@ class MainPage extends StatelessWidget {
                 ImageButton(
                     radius: 14.0,
                     size: 300,
-                    asset: "assets/images/marci.jpg",
+                    asset: "assets/images/peaky-blinders.jpg",
+                    title: "СЕРИАЛЫ",
                     func: () {}),
                 ImageButton(
                     radius: 14.0,
                     size: 300,
                     asset: "assets/images/marci.jpg",
+                    title: "ФИЛЬМЫ",
                     func: () {})
               ],
             )),
         appBar: AppBar(
           backgroundColor: Colors.redAccent,
-          title: const Text("Bar"),
+          title: Text("Shower"),
+          actions: [
+            IconButton(icon: Icon(Icons.account_circle_sharp), onPressed: (){},)
+          ],
+          ),
         ),
-      ),
-    );
+      );
   }
 }
 
 class ImageButton extends StatelessWidget {
   final double radius;
-  final int size;
+  final double size;
   final String asset;
+  final String title;
   final Function func;
 
   const ImageButton(
@@ -66,6 +72,7 @@ class ImageButton extends StatelessWidget {
       required this.radius,
       required this.size,
       required this.asset,
+        required this.title,
       required this.func})
       : super(key: key);
 
@@ -73,9 +80,24 @@ class ImageButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return IconButton(
       icon: ClipRRect(
-          borderRadius: BorderRadius.circular(14.0),
-          child: Image.asset("assets/images/marci.jpg")),
-      iconSize: 300,
+          borderRadius: BorderRadius.circular(radius),
+          child: Stack(
+            children: [
+              Image.asset(asset),
+              Align(alignment: Alignment.center, child: Opacity(opacity: 0.5 ,child: Text(
+                title,
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                    background: Paint()
+                      ..strokeWidth = 25.0
+                      ..color = Colors.white
+                      ..style = PaintingStyle.stroke
+                      ..strokeJoin = StrokeJoin.round),
+              )))
+            ],
+          )),
+      iconSize: size,
       onPressed: () {
         Navigator.push(context, MaterialPageRoute(builder: (context) {
           return const SearchPage();
